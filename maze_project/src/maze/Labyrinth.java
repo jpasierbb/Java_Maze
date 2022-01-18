f230f28acc4aaa56a81a49f96801957dcb782a4a
atlomak <andrzej.tlomak.stud@pw.edu.pl>
2022-01-18 17:00:03 +0100

    added some test in labyrinth

--- maze_project/src/maze/Labyrinth.java
+++ maze_project/src/maze/Labyrinth.java
@@ -8,30 +8,29 @@ public class Labyrinth {
     private int height = 0;
     private int width = 0;
     private String difficulty = "";
-    private String[] difficulties = {"hard","medium","easy"}; //Pre-defined difficulties
+    private String[] difficulties = {"hard", "medium", "easy"}; //Pre-defined difficulties
     private Node baseNode = null;
     private Node outNote = null;
     private Node[][] nodesToString;
 
     /**
      * Constructor to make the Labyrint with given height and width. Difficulty is being set according to size.
+     *
      * @param width
      * @param height
      */
-    public Labyrinth(int width, int height){
-        if(width<10 || height<10){
-            throw new WrongSizeException("Too small size of the Labyrint",width,height);
+    public Labyrinth(int width, int height) {
+        if (width < 10 || height < 10) {
+            throw new WrongSizeException("Too small size of the Labyrint", width, height);
         }
         this.height = height;
         this.width = width;
-        int size = width*height;
-        if(size>100){
+        int size = width * height;
+        if (size > 100) {
             difficulty = difficulties[0];
-        }
-        else if(size>50){
+        } else if (size > 50) {
             difficulty = difficulties[1];
-        }
-        else{
+        } else {
             difficulty = difficulties[2];
         }
         generateGrid();
@@ -40,85 +39,77 @@ public class Labyrinth {
 
     /**
      * Constructor to make the Labyrint with given difficulty. Size is being set according to difficulty.
+     *
      * @param difficulty
      */
-    public Labyrinth(String difficulty){
-        if(difficulty.equals(difficulties[0])){
+    public Labyrinth(String difficulty) {
+        if (difficulty.equals(difficulties[0])) {
             height = 100;
             width = 100;
-        }
-        else if(difficulty.equals(difficulties[1])){
+        } else if (difficulty.equals(difficulties[1])) {
             height = 50;
             width = 50;
-        }
-        else if(difficulty.equals(difficulties[2])){
+        } else if (difficulty.equals(difficulties[2])) {
             height = 30;
             width = 30;
         }
         generateGrid();
     }
-    private Node[][]  generateGrid(){
-        if(height<10 || width<10){
-            throw new WrongSizeException("Too small size of the Labyrint",width,height);
+
+    private Node[][] generateGrid() {
+        if (height < 10 || width < 10) {
+            throw new WrongSizeException("Too small size of the Labyrint", width, height);
         }
         baseNode = new Node();
         Node node = baseNode;
         Node[][] Nodes = new Node[width][height];
 
-        for(int i = 0; i < width; i++){
-            for(int j = 0; j < height; j++){
-                if(i == 0 && j == 0) Nodes[i][j] = node;
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (i == 0 && j == 0) Nodes[i][j] = node;
                 else Nodes[i][j] = new Node();
             }
         }
-        for(int i = 0; i < width; i++){
-            for(int j = 0; j < height; j++){
-                if(i == 0){
-                    if(j == 0){
-                        Nodes[i][j].setRightNext(Nodes[i+1][j]);
-                        Nodes[i][j].setDownNext(Nodes[i][j+1]);
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (i == 0) {
+                    if (j == 0) {
+                        Nodes[i][j].setRightNext(Nodes[i + 1][j]);
+                        Nodes[i][j].setDownNext(Nodes[i][j + 1]);
+                    } else if (j == (height - 1)) {
+                        Nodes[i][j].setRightNext(Nodes[i + 1][j]);
+                        Nodes[i][j].setUpNext(Nodes[i][j - 1]);
+                    } else {
+                        Nodes[i][j].setRightNext(Nodes[i + 1][j]);
+                        Nodes[i][j].setUpNext(Nodes[i][j - 1]);
+                        Nodes[i][j].setDownNext(Nodes[i][j + 1]);
                     }
-                    else if(j == (height - 1)){
-                        Nodes[i][j].setRightNext(Nodes[i+1][j]);
-                        Nodes[i][j].setUpNext(Nodes[i][j-1]);
+                } else if (i == (width - 1)) {
+                    if (j == 0) {
+                        Nodes[i][j].setLeftNext(Nodes[i - 1][j]);
+                        Nodes[i][j].setDownNext(Nodes[i][j + 1]);
+                    } else if (j == (height - 1)) {
+                        Nodes[i][j].setLeftNext(Nodes[i - 1][j]);
+                        Nodes[i][j].setUpNext(Nodes[i][j - 1]);
+                    } else {
+                        Nodes[i][j].setLeftNext(Nodes[i - 1][j]);
+                        Nodes[i][j].setUpNext(Nodes[i][j - 1]);
+                        Nodes[i][j].setDownNext(Nodes[i][j + 1]);
                     }
-                    else{
-                        Nodes[i][j].setRightNext(Nodes[i+1][j]);
-                        Nodes[i][j].setUpNext(Nodes[i][j-1]);
-                        Nodes[i][j].setDownNext(Nodes[i][j+1]);
-                    }
-                }
-                else if(i == (width - 1)){
-                    if(j == 0){
-                        Nodes[i][j].setLeftNext(Nodes[i-1][j]);
-                        Nodes[i][j].setDownNext(Nodes[i][j+1]);
-                    }
-                    else if(j == (height - 1)){
-                        Nodes[i][j].setLeftNext(Nodes[i-1][j]);
-                        Nodes[i][j].setUpNext(Nodes[i][j-1]);
-                    }
-                    else{
-                        Nodes[i][j].setLeftNext(Nodes[i-1][j]);
-                        Nodes[i][j].setUpNext(Nodes[i][j-1]);
-                        Nodes[i][j].setDownNext(Nodes[i][j+1]);
-                    }
-                }
-                else{
-                    if(j == 0){
-                        Nodes[i][j].setLeftNext(Nodes[i-1][j]);
-                        Nodes[i][j].setDownNext(Nodes[i][j+1]);
-                        Nodes[i][j].setRightNext(Nodes[i+1][j]);
-                    }
-                    else if(j == (height - 1)){
-                        Nodes[i][j].setLeftNext(Nodes[i-1][j]);
-                        Nodes[i][j].setUpNext(Nodes[i][j-1]);
-                        Nodes[i][j].setRightNext(Nodes[i+1][j]);
-                    }
-                    else{
-                        Nodes[i][j].setLeftNext(Nodes[i-1][j]);
-                        Nodes[i][j].setUpNext(Nodes[i][j-1]);
-                        Nodes[i][j].setDownNext(Nodes[i][j+1]);
-                        Nodes[i][j].setRightNext(Nodes[i+1][j]);
+                } else {
+                    if (j == 0) {
+                        Nodes[i][j].setLeftNext(Nodes[i - 1][j]);
+                        Nodes[i][j].setDownNext(Nodes[i][j + 1]);
+                        Nodes[i][j].setRightNext(Nodes[i + 1][j]);
+                    } else if (j == (height - 1)) {
+                        Nodes[i][j].setLeftNext(Nodes[i - 1][j]);
+                        Nodes[i][j].setUpNext(Nodes[i][j - 1]);
+                        Nodes[i][j].setRightNext(Nodes[i + 1][j]);
+                    } else {
+                        Nodes[i][j].setLeftNext(Nodes[i - 1][j]);
+                        Nodes[i][j].setUpNext(Nodes[i][j - 1]);
+                        Nodes[i][j].setDownNext(Nodes[i][j + 1]);
+                        Nodes[i][j].setRightNext(Nodes[i + 1][j]);
                     }
                 }
             }
@@ -129,15 +120,15 @@ public class Labyrinth {
 
     @Override
     public String toString() {
-        for(int i = 0; i < width + 2; i++){
+        for (int i = 0; i < width + 2; i++) {
             System.out.print("# ");
         }
         System.out.print("\n");
-        for(Node rows[]:nodesToString){
+        for (Node rows[] : nodesToString) {
             System.out.print("# ");
-            for(Node wall: rows){
+            for (Node wall : rows) {
                 int x;
-                if(!wall.isDownWall() || !wall.isLeftWall() || !wall.isUpWall() || !wall.isRightWall()) x = 1;
+                if (!wall.isDownWall() || !wall.isLeftWall() || !wall.isUpWall() || !wall.isRightWall()) x = 1;
                 else x = 0;
                 System.out.print(x);
                 System.out.print(" ");
@@ -145,7 +136,7 @@ public class Labyrinth {
             System.out.print("#");
             System.out.print("\n");
         }
-        for(int i = 0; i < width + 2; i++){
+        for (int i = 0; i < width + 2; i++) {
             System.out.print("# ");
         }
         return "";
@@ -178,16 +169,29 @@ public class Labyrinth {
     public Node getBaseNode() {
         return baseNode;
     }
+
     public Node getOutNote() {
         return outNote;
     }
-    public void generate(){
+
+    public void generate() {
     }
 
 
     public static void main(String[] args) {
         Labyrinth L1 = new Labyrinth(10, 10);
-        //Labyrinth L2 = new Labyrinth("hard");
+        Labyrinth L2 = new Labyrinth("hard");
         System.out.println(L1);
+        System.out.println("\n");
+        L1.baseNode.setDownWall(false);
+        System.out.println(L1);
+        try {
+            Labyrinth L3 = new Labyrinth(9, 9);
+        } catch (WrongSizeException e) {
+            System.out.println(e);
+            System.out.println(e.getHeight());
+            System.out.println(e.getWidth());
+        }
+
     }
 }
